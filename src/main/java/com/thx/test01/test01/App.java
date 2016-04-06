2c34fcfe7892e989ea5da874333a08bb30efa2bf
qhh <1025970610@qq.com>
2016-04-06 10:07:51 +0800

    第一次提交

---
+++ src/main/java/com/thx/test01/test01/App.java
@@ -0,0 +1,13 @@
+package com.thx.test01.test01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
